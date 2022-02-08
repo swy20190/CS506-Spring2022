50b5453519b8e5cd18810769c42c91fb2e6137c0
swy20190 <midnightsun114514@gmail.com>
2022-02-07 21:05:41 -0500

    jaccard distance; add ValueError for unexpected input

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,16 +5,26 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
+    if len(x)==0 or len(y)==0:
+        raise ValueError("lengths must not be zero")
+    if len(x) != len(y):
+        raise ValueError("lengths must be equal")
     res = 0
     for i in range(len(x)):
         res += abs(x[i]-y[i])
     return res
 
 def jaccard_dist(x, y):
-
+    if len(x)==0 or len(y)==0:
+        raise ValueError("lengths must not be zero")
+    f1 = 0
+    x = list(set(x))
     for i in x:
         if i in y:
+            f1 += 1
+    f2 = len(set(x+y))
 
+    return 1-f1/f2
 
 def cosine_sim(x, y):
     raise NotImplementedError()
