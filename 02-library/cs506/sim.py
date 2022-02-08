fb5bc942dfa19597651d9c864b8d05167c1c3552
swy20190 <midnightsun114514@gmail.com>
2022-02-07 21:15:56 -0500

    cosine finished

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -27,6 +27,21 @@ def jaccard_dist(x, y):
     return 1-f1/f2
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x)==0 or len(y)==0:
+        raise ValueError("lengths must not be zero")
+
+    if len(x) != len(y):
+        raise ValueError("lengths must be equal")
+
+    f1 = 0
+    sqx = 0
+    sqy = 0
+    for i in range(len(x)):
+        f1 += x[i]*y[i]
+        sqx += x[i]**2
+        sqy += y[i]**2
+
+    return f1/((sqx**(1/2))*(sqy**(1/2)))
+
 
 # Feel free to add more
