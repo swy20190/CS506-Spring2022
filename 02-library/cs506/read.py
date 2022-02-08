5a8ac4398f3bb2bf79af8c2baef5dbe70fe3d852
swy20190 <midnightsun114514@gmail.com>
2022-02-07 21:38:35 -0500

    read finished

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,19 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    with open(csv_file_path) as f:
+        raw = f.readlines()
+
+    for raw_line in raw:
+        raw_list = raw_line.strip().split(',')
+        res_list = []
+        for raw_v in raw_list:
+            try:
+                res_list.append(int(raw_v))
+            except Exception:
+                res_list.append(raw_v[1:-1])
+
+        res.append(res_list)
+
+    return res
